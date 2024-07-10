cb9d16f0dd50c030c49551ce4d762a97b8fb0118
one-lithe-rune <skapusniak@lithe-runes.com>
2024-07-10 17:07:00 +0100

    Remove stopped containers after init

--- project-init.sh
+++ project-init.sh
@@ -5,8 +5,7 @@ echo ""; echo "Setting up new rails project '$project_basename'..."; echo ""
 docker compose build --build-arg "APPNAME=$project_basename"
 git checkout -b main
 
-docker compose run web bash -c "/app/in-container-scripts/setup-rails-project.sh '$project_basename'"
-docker compose down
-docker compose run web bash -c "bin/rails db:create"
+docker compose run --rm web bash -c "/app/in-container-scripts/setup-rails-project.sh '$project_basename'"
+docker compose run --rm bash -c "bin/rails db:create"
 docker compose down
 
